ac55b947ca9564255c73a82aa8d50de3bee707fc
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-27 19:06:12 +0900

    Task.jsxを作成

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,7 +2,8 @@ export default function Task({ task: { id, title, state } }) {
   return (
     <div className={`list-item ${state}`}>
       <label htmlFor='checked' className='checkbox'>
-        <input type='checkbox' name='checked' />
+        <input type='checkbox' name='checked' id={`archiveTask-${id}`} />
+        <span className='checkbox-custom'></span>
       </label>
       <label htmlFor='title' className='title'>
         <input
@@ -13,6 +14,12 @@ export default function Task({ task: { id, title, state } }) {
           placeholder='Input title'
         />
       </label>
+
+      {state !== 'TASK_ARCHIVED' && (
+        <button className='pin-button'>
+          <span className='icon-star'></span>
+        </button>
+      )}
     </div>
   );
 }
