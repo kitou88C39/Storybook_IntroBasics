f314fcc2a678e6afb3965e597ad393bf27c0aa85
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-29 14:36:02 +0900

    TaskList.stories.jsxを作成

--- src/components/TaskList.stories.jsx
+++ src/components/TaskList.stories.jsx
@@ -29,58 +29,7 @@ const Mockstore = ({ taskboxState, children }) => (
   </Provider>
 );
 
-export const Default = {
-  args: {
-    tasks: [
-      //   {
-      //     id: '1',
-      //     title: 'Task 1',
-      //     state: 'TASK_INBOX',
-      //   },
-      //   {
-      //     id: '2',
-      //     title: 'Task 2',
-      //     state: 'TASK_INBOX',
-      //   },
-      //   {
-      //     id: '3',
-      //     title: 'Task 3',
-      //     state: 'TASK_INBOX',
-      //   },
-      //スプレッド構文
-      {
-        ...TaskStories.Default.args.task,
-        id: '1',
-        title: 'Task 1',
-      },
-      {
-        ...TaskStories.Default.args.task,
-        id: '2',
-        title: 'Task 2',
-      },
-      {
-        ...TaskStories.Default.args.task,
-        id: '3',
-        title: 'Task 3',
-      },
-      {
-        ...TaskStories.Default.args.task,
-        id: '4',
-        title: 'Task 4',
-      },
-      {
-        ...TaskStories.Default.args.task,
-        id: '5',
-        title: 'Task 5',
-      },
-      {
-        ...TaskStories.Default.args.task,
-        id: '6',
-        title: 'Task 6',
-      },
-    ],
-  },
-};
+export const Default = {};
 
 export const withPinnedTasks = {
   args: {
