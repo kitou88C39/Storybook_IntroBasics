c2f06dbf4365e14d6595a8361ff2f25b574a6c82
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-30 11:08:09 +0900

    src/lib/store.jsを作成

--- src/lib/store.js
+++ src/lib/store.js
@@ -22,6 +22,12 @@ export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
     'https://jsonplaceholder.typicode.com/todos?userId=1'
   );
   const data = await response.json();
+
+  const result = data.map((task) => ({
+    id: `${task.id}`,
+    title: task.title,
+    state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+  }));
 });
 
 export const TaskSlice = createSlice({
